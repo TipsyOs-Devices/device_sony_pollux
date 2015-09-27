560d5e0bd29ee05fb43bb22d8ff507dccb7052df
Alx31 <alex31.couderc@gmail.com>
2015-09-27 15:33:18 +0200

    tipsy bringup

--- tipsy.mk
+++ tipsy.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/sony/pollux/full_pollux.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/tipsy/config/nfc_enhanced.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321/SGP321:5.1.1/10.7.A.0.222/2921970873:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 5.1.1 10.7.A.0.222 2921970873 test-keys"
 
-PRODUCT_NAME := cm_pollux
+PRODUCT_NAME := tipsy_pollux
 PRODUCT_DEVICE := pollux
